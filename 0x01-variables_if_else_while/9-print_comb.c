3ccb628cf5b3458888725d7e1fa413008a5df093
“Jonah <etuafuljonah@gmail.com>
2023-06-16 18:09:46 -0700

    new mending

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,21 +9,24 @@
 int main(void)
 
 {
+
 	int n;
 
 	for (n = 48; n < 58; n++)
+
 	{
+
 		putchar(n);
 
-		if (n != 57)
+	if (n != 57)
 
-		{	
-	
-			putchar(',');
-			
-			putchar(' ');
-	
-		}
+	{
+
+		putchar(',');
+
+		putchar(' ');
+
+	}
 
 	}
 
