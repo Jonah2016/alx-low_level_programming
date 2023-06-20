5398ccf28a327e225cb5ce07df04e08a5d11c991
“Jonah <etuafuljonah@gmail.com>
2023-06-20 15:37:07 -0700

    fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
-		}
+		
 		if (k < 10 && j != 0)
 		{
 			_putchar(',');
@@ -29,6 +29,7 @@ void times_table(void)
 			_putchar(' ');
 			_putchar((k / 10) + '0');
 			_putchar((k % 10) + '0');
+		}
 		}
 	}
 	_putchar('\n');
