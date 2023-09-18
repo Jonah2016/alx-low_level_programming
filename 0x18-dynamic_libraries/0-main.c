0f5876ccdccfc23e0e9b7c45fba4b3886e47e6c4
“Jonah <etuafuljonah@gmail.com>
2023-09-18 14:46:34 -0700

    this is the fisrt full push hopefully no errors :)

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("my function to create a shared file");
+}
